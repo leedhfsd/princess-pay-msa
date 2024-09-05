6e2540420a68d697b953ad0b28b34ab06c079bb1
‘leedhfsd’ <leedhfsd@gmail.com>
2024-09-06 06:32:46 +0900

    feat:init backend

---
+++ store/src/test/java/com/princess_payment/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.princess_payment.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
